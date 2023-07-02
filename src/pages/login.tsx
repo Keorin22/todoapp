634a21cb2bc26d5c008f4dc70bcd31d024cdf7d0
keorin22 <korotenkoiy@gmail.com>
2023-07-02 21:49:28 +0700

    authorization

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,27 +1,50 @@
 import React, { useState } from 'react';
+import {  useSelector } from 'react-redux';
+import { authReducer, fetchAuth } from '../store/auth';
+import { FormValues, StateAuth } from '../store/types';
+import { useAppDispatch } from '../utils/appDispatch';
+import { Navigate } from 'react-router-dom';
+import { RootState } from '../store/store';
 
-type FormValues = {
-  email: string;
-  password: string;
-}
 
 const LoginPage = () => {
   const [formValues, setFormValues] = useState<FormValues>({ email: '', password: '' });
-
+  const dispatch = useAppDispatch()
+  // const auth = useSelector(authReducer)
+  const isAuth = useSelector((state: RootState) => state.auth.isAuth)
+  const userData = useSelector((state: RootState) => state.auth.data)
+  console.log(userData)
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setFormValues(prevValues => ({ ...prevValues, [name]: value }));
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    // Здесь можно написать логику авторизации пользователя
+  const handleSubmit = async (formValues: FormValues) => {
+    // event.preventDefault();
+    const data = await dispatch(fetchAuth({email:formValues.email, password:formValues.password}))
+    // console.log(data)
+    // console.log(1)
+    console.log(isAuth)
+    console.log(userData)
+    if (!data.payload) {
+      return alert('Не удалось авторизоваться!');
+    }
+    if ('token' in data.payload) {
+      window.localStorage.setItem('token', data.payload.token);
+      console.log('yspeh')
+    }
   };
+  
+
+
+if (isAuth) {
+  return <Navigate to="/" />;
+}
 
   return (
     <div>
       <h1>Login Page</h1>
-      <form onSubmit={handleSubmit}>
+      
         <div>
           <label htmlFor="email">Email:</label>
           <input
@@ -42,8 +65,8 @@ const LoginPage = () => {
             onChange={handleChange}
           />
         </div>
-        <button type="submit">Login</button>
-      </form>
+        <button onClick={ () => handleSubmit(formValues)}>Login</button>
+      
     </div>
   );
 };
